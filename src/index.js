bf2f81823e92ca6f0661a39089abff8af57593ed
unknown <ptownderek@yahoo.com>
2023-04-24 20:00:55 -0700

    basic store functionality in console

--- src/index.js
+++ src/index.js
@@ -3,12 +3,48 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore } from 'redux';
+
+// DISPATCH THE ACTION TO THE REDUCER WHICH MODIFIES THE STORE
+
+
+
+//ACTION = transforms state (increment/decrement, like/dislike) describe what you want to do
+// function that returns an object
+
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+//REDUCER = checks which **action** then modifies **store**
+const counter = (state = 0, action) => {
+
+  // based on the name of the action (increment or decrement)
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+
+}
 
 //store = globalized state, accesible in every component
+let store = legacy_createStore(counter);
 
+// Display in console
+store.subscribe(() => console.log(store.getState()))
 
-
-
+//DISPATCH = calls the **action**
+store.dispatch(increment());
 
 
 
