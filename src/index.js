ed973c2c0268941b1f81686238b17b5c0ed330c5
unknown <ptownderek@yahoo.com>
2023-04-24 20:35:18 -0700

    setting up new file structure, created counter/logged reducer

--- src/index.js
+++ src/index.js
@@ -3,49 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { legacy_createStore } from 'redux';
-
-// DISPATCH THE ACTION TO THE REDUCER WHICH MODIFIES THE STORE
-
-
-
-//ACTION = transforms state (increment/decrement, like/dislike) describe what you want to do
-// function that returns an object
-
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-//REDUCER = checks which **action** then modifies **store**
-const counter = (state = 0, action) => {
-
-  // based on the name of the action (increment or decrement)
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-
-}
-
-//store = globalized state, accesible in every component
-let store = legacy_createStore(counter);
-
-// Display in console
-store.subscribe(() => console.log(store.getState()))
-
-//DISPATCH = calls the **action**
-store.dispatch(increment());
-
 
 
 
@@ -63,3 +20,46 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+// import { legacy_createStore } from 'redux';
+
+// // ACTION = transforms state (increment/decrement, like/dislike) describe what you want to do
+// const increment = () => {
+//   // function that returns an object
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   // function that returns an object
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// // REDUCER = checks which **action** then modifies **store**
+// const counter = (state = 0, action) => {
+//   // based on the name of the action (increment or decrement)
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+
+// }
+// // STORE = globalized state, accesible in every component
+// let store = legacy_createStore(counter);
+
+// // Display in console
+// store.subscribe(() => console.log(store.getState()))
+
+// //DISPATCH THE ACTION TO THE REDUCER WHICH MODIFIES THE STORE
+// //DISPATCH = calls the **action**
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(increment());
+
